d682c1aac24da35bbd449ff9225f329efc34fcc9
VeraMendes <veramendes10@gmail.com>
2020-03-23 16:05:42 -0700

    day1

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -7,13 +7,24 @@ class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        # in queue, we always add to the tail
+        self.storage.add_to_tail(value)
+        self.size += 1
 
     def dequeue(self):
-        pass
+        # checking for cases where there is no elements in the queue
+        if self.size == 0:
+            return None
+        # when removing from queue, remove from head
+        else:
+            next = self.storage.head.value
+            self.storage.remove_from_head()
+            self.size -= 1
+            return next
 
     def len(self):
-        pass
+        # size of queue
+        return self.size
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -6,13 +6,24 @@ class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        # in stack, we always add to the tail
+        self.storage.add_to_tail(value)
+        self.size += 1
 
     def pop(self):
-        pass
+        # checking for cases where there is no elements in the stack
+        if self.size == 0:
+            return None
+        # when removing from stack, remove from tail
+        else:
+            prev = self.storage.tail.value
+            self.storage.remove_from_tail()
+            self.size -= 1
+            return prev
 
     def len(self):
-        pass
+        # size of stack
+        return self.size
